258002fc6b09f945c353c6c1f28571669145122d
JonMercer <mercer.jon@gmail.com>
2015-09-05 20:56:46 -0700

    Initial Commit

---
+++ Objective-C_Essential_Training/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Objective-C_Essential_Training
+//
+//  Created by Odin on 2015-09-05.
+//  Copyright (c) 2015 Jon Mercer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
