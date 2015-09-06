9205a3ac2ccae9105ee05ce77b58d1da4b73d829
JonMercer <mercer.jon@gmail.com>
2015-09-06 12:50:13 -0700

    CLEAN WIPE

--- Objective-C_Essential_Training/main.m
+++ Objective-C_Essential_Training/main.m
@@ -8,46 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
-void sampleFunction(void);
-
-void passingObject(NSString* x);
-
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
-        
-        //String literals and logging
-        int num = 5;
-        NSLog(@"Number is %i", num);
-        
-        @autoreleasepool {
-            if (num == 5) {
-                NSLog(@"Entered the if statement");
-            }
-            
-            sampleFunction();
-            
-            
-            
-            NSString *myString = @"This is my string";
-            passingObject(myString);
-            
-            
-            
-            
-        }
     }
     return 0;
-}
-
-void sampleFunction(void) {
-    for(int i = 1 ; i < 3 ; i++) {
-        NSLog(@"Inside While");
-    }
-}
-
-void passingObject(NSString* x) {
-    NSLog(@"The string is: %@", x);
 }
