d2eb472fcf5fe4c0f02cbac149b795d9457f7634
JonMercer <mercer.jon@gmail.com>
2015-09-06 09:11:27 -0700

    Function declarations

--- Objective-C_Essential_Training/main.m
+++ Objective-C_Essential_Training/main.m
@@ -8,6 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
+void sampleFunction(void);
+
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
@@ -16,6 +19,21 @@ int main(int argc, const char * argv[]) {
         //String literals and logging
         int num = 5;
         NSLog(@"Number is %i", num);
+        
+        @autoreleasepool {
+            if (num == 5) {
+                NSLog(@"Entered the if statement");
+            }
+            
+            sampleFunction();
+            
+        }
     }
     return 0;
+}
+
+void sampleFunction(void) {
+    for(int i = 1 ; i < 3 ; i++) {
+        NSLog(@"Inside While");
+    }
 }
