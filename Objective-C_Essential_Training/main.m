28badcbdc9012e70439174807f108a05c9fc00a5
JonMercer <mercer.jon@gmail.com>
2015-09-05 21:00:54 -0700

    logging string literals

--- Objective-C_Essential_Training/main.m
+++ Objective-C_Essential_Training/main.m
@@ -12,6 +12,10 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        
+        //String literals and logging
+        int num = 5;
+        NSLog(@"Number is %i", num);
     }
     return 0;
 }
